9323bbdb32649e49026aa1c136227df1f4d6e832
bulat <bulatxajrutdinoff@yandex.ru>
2023-08-31 15:03:14 +0300

    test ask_create_blog() with side_effect

--- tests/system/test_app.py
+++ tests/system/test_app.py
@@ -12,7 +12,7 @@ class TestApp(TestCase):
                 mocked_print_blogs.assert_called()
 
 
-    @patch('builtins.input')
+    @patch('builtins.input', return_value='q')
     def test_menu_prints_prompt(self, mocked_input):
         app.menu()
         mocked_input.assert_called_with(app.MENU_PROMPT)
@@ -29,4 +29,11 @@ class TestApp(TestCase):
         app.blogs = {'Test': b}
         app.print_blogs()
         mocked_print.assert_called_with('- Test Blog by Test Author (0 posts)')
+
+    def test_ask_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Test Author')
+            app.ask_create_blog()
+            # mocked_input.assert_called_with('Enter blog title: ')
+            self.assertIsNotNone(app.blogs.get('Test'))
 
